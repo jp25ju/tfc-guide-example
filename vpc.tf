671c49517ae51634748efa9ad3938153f678ea2f
jp25ju <108044111+jp25ju@users.noreply.github.com>
2022-06-23 13:35:33 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_subnet1" {
   vpc_id = aws_vpc.simple_vpc.id
   cidr_block = "10.0.0.0/24"
 
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "us-west-1a"
 
   tags = {
     Name = "public_subnet1"
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet2" {
   vpc_id = aws_vpc.simple_vpc.id
   cidr_block = "10.0.1.0/24"
 
-  availability_zone = "ap-northeast-2b"
+  availability_zone = "us-west-1c"
 
   tags = {
     Name = "public_subnet2"
